5211d41fb1c0e722f78a9a2d55bd12b92f9d634e
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-14 18:10:44 -0300

    First request

--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Pagination from 'components/Pagination';
 import MovieCard from 'components/MovieCard';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
 
 export default function Listing() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+        console.log(response.data);
+    })
+
     return (
 
         <>
