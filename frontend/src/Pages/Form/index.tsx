39259a2c2f0368679aa14f0b7f09e3f65c939dca
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-14 21:22:11 -0300

    Fixing project 2

--- frontend/src/Pages/Form/index.tsx
+++ frontend/src/Pages/Form/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
 import FormCard from 'components/FormCard';
 
 export default function Form() {
