73898ac3995bfa77c7a285a53e5445b3d9a27be1
Jhon9191 <joaoantonio15161516@gmail.com>
2022-01-14 21:19:20 -0300

    Fixing project

--- frontend/src/Pages/Form/index.tsx
+++ frontend/src/Pages/Form/index.tsx
@@ -3,10 +3,6 @@ import { useParams } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import FormCard from 'components/FormCard';
 
-type Props = {
-    movie : Movie,
-}
-
 export default function Form() {
 
     const params = useParams();
